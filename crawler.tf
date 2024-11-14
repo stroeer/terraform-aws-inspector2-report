14463be45c35a4f8e5cab153144a1c309b06516d
Saef Taher <STaher@stroeer.de>
2024-11-14 12:55:07 +0100

    Only log schema changes

--- crawler.tf
+++ crawler.tf
@@ -8,8 +8,8 @@ resource "aws_glue_crawler" "s3_crawler" {
   }
 
   schema_change_policy {
-    update_behavior = "UPDATE_IN_DATABASE"
-    delete_behavior = "DELETE_FROM_DATABASE"
+    update_behavior = "LOG"
+    delete_behavior = "LOG"
   }
   recrawl_policy {
     recrawl_behavior = "CRAWL_EVERYTHING"
